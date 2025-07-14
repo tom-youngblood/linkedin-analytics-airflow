6c87891179aa9dbc9d5c4067fa9f2a0683b853cc
tom-youngblood <tomporteryoungblood@gmail.com>
2025-07-14 09:15:20 -0700

    Fix error: exit(0) on no posts to enrich.

--- include/enrich_posts.py
+++ include/enrich_posts.py
@@ -17,24 +17,24 @@ def main():
     logger.info("Running get_unenriched_posts_from_db...")
     unenriched_posts_df = get_unenriched_posts_from_db()
     if unenriched_posts_df.empty:
-        logger.info("No unenriched posts to process. Exiting pipeline.")
-        exit(0)
+        logger.info("No unenriched posts to process. Task completed successfully.")
+        return
     logger.info("get_unenriched_posts_from_db run complete")
 
     # Scrape post media and additional metrics
     logger.info("Running prepare_media_enrichment_data...")
     media_enrichment_df = prepare_media_enrichment_data(unenriched_posts_df)
     if media_enrichment_df.empty:
-        logger.info("No media enrichment data. Exiting pipeline.")
-        exit(0)
+        logger.info("No media enrichment data. Task completed successfully.")
+        return
     logger.info("prepare_media_enrichment_data run complete")
 
     # Produce final output
     logger.info("Running finalize_enrichment_output...")
     final_enrichment_df = finalize_enrichment_output(unenriched_posts_df, media_enrichment_df)
     if final_enrichment_df.empty:
-        logger.info("No final output to ingest. Exiting pipeline.")
-        exit(0)
+        logger.info("No final output to ingest. Task completed successfully.")
+        return
     logger.info("finalize_enrichment_output run complete")
 
     # Ingest final output to PostgreSQL
